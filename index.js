f2d399ca655433ca363a475b1dff3787143557e3
PedroAT79 <plat1505@gmail.com>
2022-09-26 21:30:09 +0200

    Primeros Pasos con Node y JavaScript

---
+++ index.js
@@ -0,0 +1,12 @@
+//Este es el mensaje de bienvenida
+console.log("Esta es la puerta de entrada al Proyecto");
+
+/*Esta es la parte intermedia de nuestro proyecto:
+En esta parte realizamos los chequeos necesarios
+Por ultimo vamos a la ultima linea de saludo
+*/
+
+//Este es el mensaje que se envia para despedir:
+console.log("Adios");
+
+//Enlace al repositorio del curso: https://github.com/Open-Bootcamp/JavaScript-Basico
